2e09505613fa92a63b41d401ca09c70476940958
Adib Mosharrof <adib.mosharrof@gmail.com>
2023-03-26 14:07:37 -0400

    initialize project

---
+++ tests/test_dialog.py
@@ -0,0 +1,30 @@
+import os
+from pathlib import Path
+import random
+import glob
+import sys
+
+sys.path.append("./src")
+from dstc_dataclasses import DstcDialog
+
+import dstc_utils
+
+
+def test_data_exists():
+    data_root_dir = Path("data")
+    assert data_root_dir.exists()
+    data_dir = data_root_dir / "dstc8-schema-guided-dialogue"
+    assert data_dir.exists()
+
+
+def test_load_dialog():
+    data_dir = Path("data/dstc8-schema-guided-dialogue")
+    step = "dev"
+
+    dialog_paths = glob.glob(str(data_dir / step) + "/dialogues*.json")
+    assert len(dialog_paths) > 0
+    file = random.choice(dialog_paths)
+    dialog_json = dstc_utils.read_json(file)
+    dialog = DstcDialog.from_dict(dialog_json[0])
+    assert dialog is not None
+    return dialog
---
+++ tests/test_schema.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+import random
+import glob
+import sys
+
+import yaml
+
+sys.path.append("./src")
+
+from dstc_constants import DstcSpecialTokens, DstcConstants
+
+from dstc_dataclasses import DstcDialog, DstcSchema
+
+import dstc_utils
+
+
+def test_load_schema():
+    step = "dev"
+    data_dir = Path("data/dstc8-schema-guided-dialogue")
+    schema_paths = glob.glob(str(data_dir / step) + "/schema.json")
+    assert len(schema_paths) > 0
+    schema = random.choice(schema_paths)
+    schema_json = dstc_utils.read_json(schema)
+    assert len(schema_json) > 0
+    dstc_schema = DstcSchema.from_dict(schema_json[0])
+    schema_str = dstc_schema.get_full_repr()
+    assert schema_str is not None
